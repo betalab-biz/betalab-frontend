0fe0204dda41dcf16a32189ce3b0f687be1fa0c4
문세종(RAIL) <78732904+jongse7@users.noreply.github.com>
2025-08-06 19:53:31 +0900

    feat: Searchbar, PostCard, PostCardMini Navibar 컴포넌트 제작 ✨ Feature (#20)
    * chore: main 최신화
    
    * feat: searchbar
    
    * feat: 컴포넌트 개발
    
    * feat: header에 className 추가
    
    * chore: conflict 해결
    
    * fix: tag의 onclick을 nullable하게

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,17 @@
-import type { Preview } from '@storybook/nextjs';
-import '@/styles/globals.css';
-import '@/styles/tailwind.css';
+import type { Preview } from "@storybook/nextjs";
+import "@/styles/globals.css";
+import "@/styles/tailwind.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
+    },
+    nextjs: {
+      appDirectory: true,
     },
   },
 };
