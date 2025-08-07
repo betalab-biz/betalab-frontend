f44546092177d157e93b66626d29702794d92fe7
Sumin <145013061+psm1st@users.noreply.github.com>
2025-08-07 13:02:03 +0900

    [Feat] test add page category 분기 이동 (#24)
    * fix : svg 문제 해결
    
    * feat : carousel component 구현
    
    * feat : carousel component storybook에 등록
    
    * rename : storybook error 수정
    
    * fix : carouselBar 디자인 수정 및 완료
    
    * feat : 테스트 이미지 등록
    
    * feat : common component를 활용한 next button 구현
    
    * feat : common component를 활용한 category selector 구현
    
    * feat : 테스트 종류 페이지 구현
    
    * fit : code review 반영해 size-20으로 변경
    
    * fix : atom design에 맞게 경로 수정
    
    * fix : atom design으로 인한 파일 충돌 해결
    
    * feat : category 별 이동을 위한 카테고리 mapping
    
    * feat : 카테고리 별로 이동 로직 구현
    
    * feat : app 플랫폼 선택 페이지 구현
    
    * chore : prettier 설정
    
    * feat : game 플랫폼 선택 페이지 구현
    
    * feat : 임시저장 기능 구현 - localstorage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,16 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  addons: [],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  staticDirs: ['../public'],
 };
 export default config;
